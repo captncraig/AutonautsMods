b0dad29efa57f7107e16d521dbc0a8f9e109c935
Craig Peterson <captncraig@users.noreply.github.com>
2019-10-23 11:31:10 -0400

    initial

---
+++ SuperBots/Main.cs
@@ -0,0 +1,37 @@
+﻿using Harmony;
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityModManagerNet;
+
+
+namespace SuperBots
+{
+    internal static class Main
+    {
+        public static UnityModManager.ModEntry mod;
+        
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            mod = modEntry;
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.Patch(
+                original: AccessTools.Method(typeof(Worker), nameof(Worker.GetTotalMaxInstuctions)),
+                prefix: new HarmonyMethod(typeof(Main), nameof(Main.GetTotalMaxInstuctions))
+            );
+
+            Type type = typeof(Worker);
+            FieldInfo info = type.GetField("m_LoseEnergy", BindingFlags.NonPublic | BindingFlags.Static);
+            info.SetValue(null, false);
+            Debug.Log("MOD LOADED");
+            return true;
+        }
+
+        static bool GetTotalMaxInstuctions(Worker __instance, ref int __result)
+        {
+            Debug.Log("HARMONY PATCH CALLED");
+            __result = 100;
+            return false;
+        }
+    }
+}

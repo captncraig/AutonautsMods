222a0892739900b844462cea1f8ec3d0953cebed
Craig Peterson <captncraig@users.noreply.github.com>
2019-10-23 16:05:32 -0400

    Working!

--- SuperBots/Main.cs
+++ SuperBots/Main.cs
@@ -1,13 +1,11 @@
 ﻿using Harmony;
 using System;
 using System.Reflection;
-using UnityEngine;
 using UnityModManagerNet;
-
 
 namespace SuperBots
 {
-    internal static class Main
+    public static class Main
     {
         public static UnityModManager.ModEntry mod;
         
@@ -16,22 +14,22 @@ namespace SuperBots
             mod = modEntry;
             var harmony = HarmonyInstance.Create(modEntry.Info.Id);
             harmony.Patch(
-                original: AccessTools.Method(typeof(Worker), nameof(Worker.GetTotalMaxInstuctions)),
-                prefix: new HarmonyMethod(typeof(Main), nameof(Main.GetTotalMaxInstuctions))
-            );
+                original: AccessTools.Method(typeof(TeachWorkerScriptEdit),
+                                            nameof(TeachWorkerScriptEdit.GetFreeMemory)),
+                postfix: new HarmonyMethod(typeof(Main), nameof(GetFreeMemory))
+                );
 
             Type type = typeof(Worker);
             FieldInfo info = type.GetField("m_LoseEnergy", BindingFlags.NonPublic | BindingFlags.Static);
             info.SetValue(null, false);
-            Debug.Log("MOD LOADED");
+
             return true;
         }
 
-        static bool GetTotalMaxInstuctions(Worker __instance, ref int __result)
+        [HarmonyPostfix]
+        static void GetFreeMemory(ref int __result)
         {
-            Debug.Log("HARMONY PATCH CALLED");
             __result = 100;
-            return false;
         }
     }
 }
